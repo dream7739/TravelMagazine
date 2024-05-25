bf5e2741017ea367743493cb04af41c58afb3a7d
홍정민 <dream7739@naver.com>
2024-05-26 00:42:04 +0900

    해시태그 버튼 통해 검색 추가

--- TravelMagazine/RestaurantViewController.swift
+++ TravelMagazine/RestaurantViewController.swift
@@ -11,10 +11,18 @@ class RestaurantViewController: UIViewController, UITableViewDelegate, UITableVi
     
     @IBOutlet var searchTextField: UITextField!
     @IBOutlet var searchButton: UIButton!
+    
+    @IBOutlet var koreanButton: UIButton!
+    @IBOutlet var chineseButton: UIButton!
+    @IBOutlet var westernButton: UIButton!
+    @IBOutlet var etcButton: UIButton!
+    @IBOutlet var tagButtonCollection : [UIButton]!
+    
     @IBOutlet var restaurantTableView: UITableView!
     
     private let restaurantList = RestaurantList().restaurantArray
     private var filteredList:[Restaurant] = []
+    private let categoryTitleList = ["한식", "중식", "양식", "기타"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,16 +33,13 @@ class RestaurantViewController: UIViewController, UITableViewDelegate, UITableVi
         
         searchTextField.clearButtonMode = .whileEditing
         
+        for (idx, button) in tagButtonCollection.enumerated(){
+            designButton(button, idx)
+        }
+        
         filteredList = restaurantList
     }
     
-    func filterRestaurant(_ searchText: String){
-        filteredList = restaurantList.filter{
-            $0.name.lowercased().contains(searchText)
-        }
-        
-        restaurantTableView.reloadData()
-    }
     
     @IBAction func searchTextChanged(_ sender: UITextField) {
         //앞 뒤 공백을 제외한 단어를 검색어로 지정
@@ -44,12 +49,54 @@ class RestaurantViewController: UIViewController, UITableViewDelegate, UITableVi
         //2. 검색어가 ""면 restaurantList를 보여준다.
         //3. 중복으로 restaurantList값을 보여주지 않기위해서 count 체크를 해서 전체배열과 같으면 갱신하지 않는다.
         
-        if !searchText.isEmpty{
+        if !searchText.isEmpty {
             filterRestaurant(searchText)
         }else if filteredList.count != restaurantList.count{
             filteredList = restaurantList
             restaurantTableView.reloadData()
         }
+    }
+    
+    @IBAction func categoryButtonClicked(_ sender: UIButton) {
+        guard let title = sender.currentTitle else { return }
+        searchTextField.text = title
+
+        let category = title.replacingOccurrences(of: "#", with: "").trimmingCharacters(in: .whitespaces)
+        filterCategory(category)
+    }
+    
+    
+    private func designButton(_ sender: UIButton, _ idx: Int){
+        sender.layer.cornerRadius = 15
+        sender.layer.borderWidth = 1.5
+        sender.layer.borderColor = UIColor.systemIndigo.cgColor
+        sender.tintColor = .systemIndigo
+        
+        let buttonTitle = "# \(categoryTitleList[idx])"
+        sender.setTitle(buttonTitle, for: .normal)
+    }
+    
+    private func filterRestaurant(_ searchText: String){
+        filteredList = restaurantList.filter{
+            $0.name.lowercased().contains(searchText)
+        }
+        
+        restaurantTableView.reloadData()
+    }
+    
+    private func filterCategory(_ category: String){
+        
+        if category == "기타"{
+            filteredList = restaurantList.filter{
+                !categoryTitleList.contains($0.category)
+            }
+        }else{
+            filteredList = restaurantList.filter{
+                $0.category == category
+            }
+        }
+        
+        restaurantTableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
