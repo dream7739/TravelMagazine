38945da937f868ed6b5a04ecf0c555980c6fca13
홍정민 <dream7739@naver.com>
2024-05-25 12:10:07 +0900

    tableViewCell UI배치 tableView 구성

---
+++ TravelMagazine/TravelTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TravelTableViewCell.swift
+//  TravelMagazine
+//
+//  Created by 홍정민 on 5/25/24.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+    @IBOutlet var travelImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subtitleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+}
---
+++ TravelMagazine/TravelTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TravelTableViewController.swift
+//  TravelMagazine
+//
+//  Created by 홍정민 on 5/25/24.
+//
+
+import UIKit
+
+class TravelTableViewController: UITableViewController {
+
+    private let travelMegazine = MagazineInfo().magazine
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 450
+    }
+ 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return travelMegazine.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TravelTableViewCell", for: indexPath) as! TravelTableViewCell
+        
+        let data = travelMegazine[indexPath.row]
+        
+        cell.travelImageView.image = UIImage(named: data.photo_image)
+        cell.titleLabel.text = data.title
+        cell.subtitleLabel.text = data.subtitle
+        cell.dateLabel.text = data.date
+        
+        return cell
+    }
+    
+}
