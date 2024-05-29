060a64bd2642bff0110e569af2633da06f0caf66
홍정민 <dream7739@naver.com>
2024-05-29 17:52:04 +0900

    관광지 화면, 광고 화면 셀 데이터에 맞게 분기 push, present 화면전환 구현

---
+++ TravelMagazine/Controller/DetailCityViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailCityViewController.swift
+//  TravelMagazine
+//
+//  Created by 홍정민 on 5/29/24.
+//
+
+import UIKit
+
+class DetailCityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
